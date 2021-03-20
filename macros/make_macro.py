45d3b70829d334e3176f4d6775937a10812ffd3e
etsai <emily.tsai11@gmail.com>
2021-03-20 23:44:21 +0100

    adjust macro files

--- macros/make_macro.py
+++ macros/make_macro.py
@@ -3,7 +3,7 @@ import argparse
 # Define parser
 parser = argparse.ArgumentParser(description = "Variables to generate KLMC_SAC macro files with")
 parser.add_argument("BeamType", type = int, help = "Beam type -- Sets incident particle")
-parser.add_argument("IncidentE", type = float, help = "Energy of incident particle")
+parser.add_argument("IncidentE", type = str, help = "Energy of incident particle")
 parser.add_argument("IncidentEUnit", type = str, help = "Unit of energy of incident particle")
 parser.add_argument("NEvents", type = int, help = "Number of events")
 parser.add_argument("-r", type = int, help = "Run number")
@@ -14,11 +14,11 @@ BeamType = args.BeamType
 IncidentE = args.IncidentE
 IncidentEUnit = args.IncidentEUnit
 NEvents = args.NEvents
-RunNum = args.RunNum
+RunNum = args.r
 
 # Name of file
-filename = "SAC_" + str(BeamType) + "_" + str(int(IncidentE)) + IncidentEUnit + "_n" + str(NEvents)
-if args.RunNum != None: filename += "_r" + str(RunNum)
+filename = "SAC_" + str(BeamType) + "_" + IncidentE + IncidentEUnit + "_n" + str(NEvents)
+if RunNum != None: filename += "_r" + str(RunNum)
 
 # Write macro file
 with open(filename + ".mac", "w") as f:
@@ -33,7 +33,7 @@ with open(filename + ".mac", "w") as f:
 	f.write("/run/initialize\n")
 	f.write("\n")
 	f.write("/PrimaryGeneratorAction/BeamType " + str(BeamType) + "\n")
-	f.write("/PrimaryGeneratorAction/BeamEnergy " + str(IncidentE) + " " + IncidentEUnit + "\n")
+	f.write("/PrimaryGeneratorAction/BeamEnergy " + IncidentE + " " + IncidentEUnit + "\n")
 	f.write("\n")
 	f.write("/Analysis/FileName " + filename + "\n")
 	f.write("\n")
